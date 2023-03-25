926804c4b21873733d220ddcb8d7fabba482bc4e
indigorose <indigorose049@gmail.com>
2023-03-25 18:46:02 +0000

    Update game logic

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-/* The aim of the game is to guess the name of th pokemon based on what is stored in the pokemon API. 
+/* The aim of the game is to guess the name of the pokemon based on what is stored in the pokemon API. 
 Initially try to 'catch' 30 pokemon at random, with possible bonus points for famous pokemon.
 */
 
-// let types = [
-// 	'Normal',
-// 	'Fighting',
-// 	'Rock',
-// 	'Fire',
-// 	'Poison',
-// 	'Ghost',
-// 	'Water',
-// 	'Ground',
-// 	'Dark',
-// 	'Grass',
-// 	'Flying',
-// 	'Dragon',
-// 	'Electric',
-// 	'Psychic',
-// 	'Steel',
-// 	'Ice',
-// 	'Bug',
-// 	'Fairy',
-// ];
 // Extra Scoring Pokemon
 let famousPokemon = [
 	'Bulbasaur',
@@ -31,6 +11,7 @@ let famousPokemon = [
 	'Mew',
 	'MewTwo',
 ];
+
 // Generate random number pokemon - original 151
 function getRandomID(max) {
 	return Math.floor(Math.random() * max);
@@ -38,36 +19,48 @@ function getRandomID(max) {
 
 // Store already called Pokemon to avoid double calls
 let pokemonStore = [];
-
-// Constants
-const choice = getRandomID(151);
-const startUrl = 'https://pokeapi.co/api/v2/pokemon/' + choice;
-// 404 error with guessUrl
-// const guessUrl = `https://pokeapi.co/api/v2/type/18`;
+let answerPokemon = 'Not yet'
+let score = 0
 
 // Button Functionality
 document.getElementById('start').addEventListener('click', getPokemon);
-document.getElementById('guess').addEventListener('click', checkAnswer);
 
 function getPokemon() {
-	console.log(choice);
-    let guess = document.querySelector('input').value;
+	let choice = getRandomID(151);
+    let startUrl = 'https://pokeapi.co/api/v2/pokemon/' + choice;
+    console.log(choice);
 	fetch(startUrl)
 		.then((res) => res.json()) // parse response as JSON
 		.then((data) => {
-			console.log(data);
+			// console.log(data);
 			console.log(data.name);
 			document.querySelector('img').src = data.sprites.front_default;
-            let answerPokemon = data.name
-			// document.getElementById('pokemon-name').innerHTML = data.name
-			// console.log(getRandomID(151))
+            answerPokemon = data.name
+            console.log(answerPokemon)
 		})
 		.catch((err) => {
 			console.log(`error ${err}`);
 		});
 }
-
+document.getElementById('guessPokemon').addEventListener('click', checkAnswer);
 // Guess function to create guessing buttons
-// function checkAnswer(){
+function checkAnswer(){
+    let guess = document.querySelector('input').value.toLowerCase();
+    console.log('This is my pokemon guess ' + guess)
+    console.log('This is my pokemon answer ' + answerPokemon)
+    if (answerPokemon === guess){
+// TODO - break down actions when we have a right answer.
+        console.log('great')
+        score +=1
+        console.log(score)
+        pokemonStore.push(answerPokemon)
+        console.log(pokemonStore)
+        getPokemon()
 
-// }
+    } else {
+        console.log('wrong answer')
+        getPokemon()
+    }
+};
+// 404 error with guessUrl
+// const guessUrl = `https://pokeapi.co/api/v2/type/18`;
